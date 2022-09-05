b6c2d0625a4999460895b327d3321cafbfce5c25
mart-anthony-stark <martsalazar0@gmail.com>
2022-09-05 21:09:46 +0800

    remove doc

--- movie-theater-simu/simulate.py
+++ movie-theater-simu/simulate.py
@@ -1,11 +1,3 @@
-"""Companion code to https://realpython.com/simulation-with-simpy/
-
-'Simulating Real-World Processes With SimPy'
-
-Python version: 3.7.3
-SimPy version: 3.0.11
-"""
-
 import simpy
 import random
 import statistics
